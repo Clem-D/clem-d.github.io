b4c4e9cae7b759f8b8a08a97cf487483a5a9f2c2
Clem-D <cclementddel@gmail.com>
2020-09-30 17:16:09 +0200

    Update website

--- js/backgroundManager.js
+++ js/backgroundManager.js
@@ -1,9 +1,31 @@
 // VARIABLES
 // FUNCTIONS
-$('.gallery-img').click(function() {
-  textParamsButton.addClass('hidden');
-  $('.gallery-img').removeClass('gallery-img-clicked');
-  $(this).addClass('gallery-img-clicked');
-  imageObj.src = $(this).attr('src');
+$(".gallery-img").click(function () {
+  textParamsButton.addClass("hidden");
+  $(".gallery-img").removeClass("gallery-img-clicked");
+  $(this).addClass("gallery-img-clicked");
+  imageObj.src = $(this).attr("src");
   layer.draw();
+});
+
+$("#input-file").on("change", function (e) {
+  var URL = window.webkitURL || window.URL;
+  var url = URL.createObjectURL(e.target.files[0]);
+  // main API:
+  var img = new Image();
+  img.src = url;
+
+  img.onload = function () {
+    var yoda = new Konva.Image({
+      x: 0,
+      y: 0,
+      image: img,
+      draggable: true,
+      dragOnTop: true
+    });
+
+    layer.add(yoda);
+    layer.draw();
+  };
+
 });
--- js/palette.js
+++ js/palette.js
@@ -5,6 +5,7 @@ var addJointButton = $('#add-joint');
 var addSunGlasses = $('#add-sun-glasses');
 var addThugLife = $('#add-thug-life');
 var zoneTextID = 0;
+var imgURL = "assets/images/";
 // TODO : We should not hardcode values like that
 var xStart = 50;
 var yStart = 80;
@@ -17,24 +18,24 @@ addTextButton.click(function() {
 
 addCapButton.click(function() {
   textParamsButton.toggleClass('hidden');
-  createImage('hat.png',{xStart: 50, yStart: 80,}, true);
+  createImage(imgURL + 'hat.png',{xStart: 50, yStart: 80,}, true);
   layer.draw();
 });
 
 addJointButton.click(function() {
   textParamsButton.toggleClass('hidden');
-  createImage('joint.png',{xStart: 50, yStart: 80,}, true);
+  createImage(imgURL + 'joint.png',{xStart: 50, yStart: 80,}, true);
   layer.draw();
 });
 
 addSunGlasses.click(function() {
   textParamsButton.toggleClass('hidden');
-  createImage('glasses.png',{xStart: 50, yStart: 80,}, true);
+  createImage(imgURL + 'glasses.png',{xStart: 50, yStart: 80,}, true);
   layer.draw();
 });
 
 addThugLife.click(function() {
   textParamsButton.toggleClass('hidden');
-  createImage('thug_life.png',{xStart: 50, yStart: 80,}, true);
+  createImage(imgURL + 'thug_life.png',{xStart: 50, yStart: 80,}, true);
   layer.draw();
 });
